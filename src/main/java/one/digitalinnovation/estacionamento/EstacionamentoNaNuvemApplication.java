0dbd734d676867312289d73392c69daa335a57a0
Gui <oxemtex@gmail.com>
2022-12-13 18:30:42 -0300

    Estrutura inicial

---
+++ src/main/java/one/digitalinnovation/estacionamento/EstacionamentoNaNuvemApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.estacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionamentoNaNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionamentoNaNuvemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/estacionamento/EstacionamentoNaNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.estacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionamentoNaNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
